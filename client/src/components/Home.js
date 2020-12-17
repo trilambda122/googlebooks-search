4607387b6280a2ea19c300df5efb0fdd635dee96
trilambda122 <trilambda122>
2020-12-17 16:26:19 -0600

    pages and nav bar skeleton

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
-
+import Nav  from './Nav'
 export default function Home() {
   return (
-  <div>
-<h1>Home</h1>
-  </div>
+<React.Fragment>
+  <Nav/>
+  <h1>HOME</h1>
+</React.Fragment>
   )
 }
--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
-
+import Nav from './Nav'
 export default function Saved() {
   return (
-  <div>
-<h1>Saved</h1>
-  </div>
+<React.Fragment>
+  <Nav/>
+  <h1>Saved</h1>
+</React.Fragment>
   )
 }
