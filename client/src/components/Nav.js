6b354d1a2df3594c69ec5a59cef35d51da5e6f6a
trilambda122 <trilambda122>
2020-12-17 17:53:11 -0600

    working search form in the nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,28 +1,60 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { SEARCH } from '../utils/actions';
+import { useStoreContext } from "../utils/GlobalState";
+import api from '../utils/api'
+
+
 
 export default function Nav() {
+  
+const inputRef = useRef();
+const [state,dispatch]=useStoreContext();
+  
+const getSearchResults = async(searchStr)=>{
+  const results = await api.getBook(searchStr)
+  return results
+}
+
+
+const handleSubmit = (e)=>{
+  console.log('--------HANDLING----(NAV.js)----')
+  e.preventDefault()
+  getSearchResults(inputRef.current.value).then((result)=>{
+    dispatch({
+      type: SEARCH,
+      search: inputRef.current.value,
+      searchResults: result.data
+  
+    })
+    inputRef.current.value = ''
+    console.log(state)
+  })
+}
+
+
+
   return (
     <React.Fragment>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/saved">Saved</a>
+        <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/saved">Saved</a>
         </li>
     </ul>
     </div>
     </div>
-    <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-secondary" type="submit">Search</button>
+    <form className="d-flex" onSubmit={handleSubmit}>
+        <input className="form-control me-2"  ref={inputRef}  type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-secondary" type="submit">Search</button>
       </form>
 </nav>
 
