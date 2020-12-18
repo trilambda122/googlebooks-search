93321b1cf938e5f3c3c5113ac34595bdc5994e0f
trilambda122 <trilambda122>
2020-12-17 21:39:57 -0600

    fixed alignment issue with nav bar on desktop

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -32,23 +32,33 @@ const handleSubmit = (e)=>{
   })
 }
 
-
+{/* <img className="img-thumbnail" src='./imgs/books-75x75.png' style={{maxWidth: "75px"}}/> */}
 
   return (
     <React.Fragment>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <div className="container-fluid">
-    <a className="navbar-brand " href="#"><img className="img-thumbnail" src='./imgs/books-75x75.png'/> BOOK SEARCH</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <a className="navbar-brand" href="/">
+    <img className="img-thumbnail" src='./imgs/books-75x75.png'/>
+    </a> 
+    <a className="navbar-brand" aria-current="page" href="/">BOOK SEARCH</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    {/* <div className="container-fluid border ml-0">
+    <a class="nav-link active" aria-current="page" href="/">Home</a>
+    </div> */}
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    
         <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <a className="nav-link active navbar-text" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/saved">Saved</a>
+            <a className="nav-link active navbar-text" aria-current="page" href="/saved">Saved</a>
         </li>
     </ul>
     </div>
