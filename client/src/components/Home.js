85f3f74b3f0af1f211377bf26aa9022cef4b941c
trilambda122 <trilambda122>
2020-12-20 16:43:03 -0600

    react able to store favorites

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,43 +6,39 @@ import BookContext from '../context/books-context'
 export default function Home() {
 
 
-  const {searchResults}= useContext(BookContext)
+  const {searchResults, addBooktoFavorites, savedBooks, removeBookFromFavorites}= useContext(BookContext)
 
 
 
   return (
 <div>
   <Nav/>
-
-
 {searchResults.map((singleBook)=>{
 
-return <div className="container border-bottom border-secondary ">
-<div className="clearfix">
+return <div className="container border-bottom border-secondary " key={singleBook.id}>
+          <div className="clearfix">
 
-<a className="text-decoration-none" href={singleBook.volumeInfo.previewLink}>
-<img src={singleBook.volumeInfo.imageLinks.thumbnail} className="col-md-6 float-md-start mb-3 ms-md-3 img-effect" alt="..."/>
-</a>
+              <a className="text-decoration-none" href={singleBook.volumeInfo.previewLink}>
+              <img src={singleBook.volumeInfo.imageLinks.thumbnail} className="col-md-6 float-md-start mb-3 ms-md-3 img-effect" alt="..."/>
+              </a>
 
-<div className="ml-3">
-{/* <button className="btn btn-outline-secondary justify-content-md-end mt-2" type="submit">Save</button> */}
-
-    <p className="fs-2 fw-bold"> <img  className="save-icon "src='./imgs/star.svg'/> {singleBook.volumeInfo.title}</p>
-    <p className="fs-5">{singleBook.volumeInfo.authors}</p>
-    <p className="fs-6 fw-light">{singleBook.volumeInfo.publisher}  {singleBook.volumeInfo.publishedDate}</p>
-  <p className="fs-6">{singleBook.volumeInfo.description}</p>
-</div>
-
-
-
-
-
-</div>
+              <div className="ml-3">
+                  <p className="fs-2 fw-bold"> 
+                  {savedBooks.map(book => book.id).includes(singleBook.id)   ?  <img  className="save-icon "src='./imgs/stared.svg' onClick={()=>{removeBookFromFavorites(singleBook)}} alt="Favorite"/>  :  <img  className="save-icon "src='./imgs/star.svg' onClick={()=>{addBooktoFavorites(singleBook)}} alt="Could be a favorite"/>
+                  
+                  }
+                  {singleBook.volumeInfo.title}</p>
+                  <p className="fs-5">{singleBook.volumeInfo.authors}</p>
+                  <p className="fs-6 fw-light">{singleBook.volumeInfo.publisher}  {singleBook.volumeInfo.publishedDate}</p>
+                <p className="fs-6">{singleBook.volumeInfo.description}</p>
+              </div>
+          </div>
 </div>
 
 })}
 </div>
   )
+ 
 }
 
 // title: { type: String },
