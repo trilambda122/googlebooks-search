345fa05e31379fb289aff6d095876d6c3749cf41
trilambda122 <trilambda122>
2020-12-22 16:39:35 -0600

    updated error pages

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -5,7 +5,20 @@ export default function NotFound() {
   return (
     <div>
       <Nav/>
-      <h1>No restuls found</h1>
+<div>
+<div className="page-wrap d-flex flex-row align-items-center">
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-md-12 text-center">
+                <span className="display-1 d-block">No Results</span>
+                <div className="mb-4 lead">What you searched for returned no results please try again</div>
+                {/* <a href="/" className="btn btn-link">Back to Home</a> */}
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
     </div>
   )
 }
